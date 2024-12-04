7a9ba371d2795b6af857abe43473037c17ee8ac4
anjelika A <vbfbbf@fg.com>
2024-12-04 16:39:45 +0300

    Revert "Revert "first commit""
    This reverts commit 6c388a82b5dc22d949bad5877b4436852774ddbf.

---
+++ src/BmiService.java
@@ -0,0 +1,11 @@
+public class BmiService {
+
+
+        public int calculate(int weightKg, double heightMeters) {
+            double bmiIndex = weightKg / Math.pow (heightMeters, 2);
+            return (int) bmiIndex;
+
+        }
+
+
+}
